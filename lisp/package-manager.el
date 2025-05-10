39e422da649185f245b127f26ee6b1b6a1e934b7
Alex Salek <alexsalek2007@gmail.com>
2025-05-10 13:15:02 +0100

    multiple cursors + whitespace

--- lisp/package-manager.el
+++ lisp/package-manager.el
@@ -1,54 +1,69 @@
 (setq package-archives
       '(("melpa" . "https://melpa.org/packages/")
-        ("gnu" . "https://elpa.gnu.org/packages/")))
+        ("gnu" . "https://elpa.gnu.org/packages/"))
+)
 
 (package-initialize)
 (unless package-archive-contents
-  (package-refresh-contents))
+  (package-refresh-contents)
+)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
-  (package-install 'use-package))
+  (package-install 'use-package)
+)
 
 (eval-when-compile
-  (require 'use-package))
+  (require 'use-package)
+)
 
 (use-package magit
   :ensure t
   :commands (magit-status magit-log-all)
-  :defer t)
+  :defer t
+)
 
 (use-package multiple-cursors
   :ensure t
-  :commands (mc/edit-lines mc/mark-all-dwim)
-  :defer t)
+  :bind (("C-S-c C-S-c" . mc/edit-lines)
+         ("C->"         . mc/mark-next-like-this)
+         ("C-<"         . mc/mark-previous-like-this)
+         ("C-c C-<"     . mc/mark-all-like-this)
+  )
+)
 
 (use-package vterm
   :ensure t
   :commands (vterm vterm-other-window)
-  :defer t)
+  :defer t
+)
 
 (use-package company
   :ensure t
   :hook (after-init . global-company-mode)
-  :defer t)
+  :defer t
+)
 
 (use-package lsp-mode
   :ensure t
   :defer t
   :hook ((python-mode . lsp)
          (c-mode . lsp)
-         (cpp-mode . lsp)))
+         (cpp-mode . lsp)
+  )
+)
 
 (use-package flycheck
   :ensure t
   :defer t
-  :hook (lsp-mode . flycheck-mode))
+  :hook (lsp-mode . flycheck-mode)
+)
 
 (use-package lsp-ui
   :ensure t
   :after lsp-mode
-  :defer t)
+  :defer t
+)
 
 (use-package smex
   :ensure t
@@ -63,12 +78,14 @@
   :init
   (ido-mode t)
   (ido-everywhere 1)
-  (ido-ubiquitous-mode 1))
+  (ido-ubiquitous-mode 1)
+)
 
 (use-package indent-guide
   :ensure t
   :commands indent-guide-mode
-  :defer t)
+  :defer t
+)
 
 
 (provide 'package-manager)
